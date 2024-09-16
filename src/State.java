6780276369600fedb393b86ff7becf86e455c0e0
Nguyễn Minh <138985110+Ming191@users.noreply.github.com>
2024-09-16 22:30:08 +0700

    Test5

--- src/State.java
+++ src/State.java
@@ -1,3 +1,13 @@
+import java.util.HashMap;
+
 public class State {
-    
+    public enum states {
+        MAIN_MENU,
+        EXIT(0),
+        ADD(1),
+        REMOVE(2),
+        UPDATE(3)
+        DISPLAY(4),
+        LOOKUP(5),
+    }
 }
