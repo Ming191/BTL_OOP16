8cf560a651fd73e40d149422a62f28d6f50c208a
Nguyễn Minh <138985110+Ming191@users.noreply.github.com>
2024-09-16 22:21:59 +0700

    Test4

---
+++ src/Word.java
@@ -0,0 +1,21 @@
+// Package not detected, please report project structure on CodeTogether Live's GitHub Issues
+
+public class Word {
+    private String wordTarget;
+    private String wordExplain;
+    Word(String wordTarget, String wordExplain){
+
+    }
+    Word(){
+
+    }
+    public String getWordTarget(){
+        return wordTarget;
+    }
+    public String getWordExpla(){
+            return wordTarget;
+    }
+    public void setWordTarget(String p_WordTarget){
+        this.wordTarget = p_WordTarget;
+    }
+}
