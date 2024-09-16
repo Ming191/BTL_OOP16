9a8cf8d72c71104e280c58044cea649cf082fb66
Nguyễn Minh <138985110+Ming191@users.noreply.github.com>
2024-09-16 22:09:53 +0700

    Test3

---
+++ src/DictionaryCommandline.java
@@ -0,0 +1,22 @@
+public class DictionaryCommandline {
+
+    public void showAllWords() {
+
+    }
+
+    public  void dictionaryBasic() {
+
+    }
+
+    public void dictionaryAdvance() {
+        System.out.println(
+                "======================================================"
+        );
+        System.out.println(
+                "Welcome to My Application!"
+        );
+        System.out.println(
+                "======================================================"
+        );
+    }
+}
