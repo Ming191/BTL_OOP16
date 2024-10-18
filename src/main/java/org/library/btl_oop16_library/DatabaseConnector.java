7fb4b350df7ab7e9c675103530fa3441546a60c2
Do Tien Loc <dotienloc2005@gmail.com>
2024-10-18 14:26:07 +0700

    sua 1 ti th

--- src/main/java/org/library/btl_oop16_library/DatabaseConnector.java
+++ src/main/java/org/library/btl_oop16_library/DatabaseConnector.java
@@ -61,23 +61,31 @@ public class DatabaseConnector {
 
             stmt = c.createStatement();
 
-            String query = "INSERT INTO book (id, title, author, type, language, available) VALUES (?, ?, ?, ?, ?, ?)";
-            PreparedStatement pstmt = c.prepareStatement(query);
-            try {
-                pstmt.setInt(1, book.getId());
-                pstmt.setString(2, book.getTitle());
-                pstmt.setString(3, book.getAuthor());
-                pstmt.setString(4, book.getType());
-                pstmt.setString(5, book.getLanguage());
-                pstmt.setString(6, book.getAvailable());
+            String q = "select count(*) from book where title = '" + book.getTitle() + "'";
+            ResultSet rs = stmt.executeQuery(q);
+            int count = rs.getInt(1);
+            if (count != 0) {
+                String q1 = "update book set quantity = quantity + 1";
+                stmt.executeUpdate(q1);
+            }
+            else {
+                String query = "INSERT INTO book (id, title, author, type, language, available) VALUES (?, ?, ?, ?, ?, ?)";
+                PreparedStatement pstmt = c.prepareStatement(query);
+                try {
+                    pstmt.setInt(1, book.getId());
+                    pstmt.setString(2, book.getTitle());
+                    pstmt.setString(3, book.getAuthor());
+                    pstmt.setString(4, book.getType());
+                    pstmt.setString(5, book.getLanguage());
+                    pstmt.setString(6, book.getAvailable());
 
-                pstmt.executeUpdate();
-                //rs = stmt.executeQuery(query);
+                    pstmt.executeUpdate();
+                    //rs = stmt.executeQuery(query);
+                } catch (Exception e) {
+                    System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                }
+                library.getBooks().add(book);
             }
-            catch (Exception e) {
-                System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            }
-            library.getBooks().add(book);
         }
         catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
