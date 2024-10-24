ffd375c4b68bb73bd3d160b79e3ab724b065ffb6
Do Tien Loc <dotienloc2005@gmail.com>
2024-10-24 14:07:09 +0700

    add addUserButtonOnClick

--- src/main/java/org/library/btl_oop16_library/UserViewController.java
+++ src/main/java/org/library/btl_oop16_library/UserViewController.java
@@ -2,10 +2,17 @@ package org.library.btl_oop16_library;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserViewController {
 
@@ -49,7 +56,22 @@ public class UserViewController {
     private TableView<User> table;
 
     @FXML
-    void addUserButtonOnClick(ActionEvent event) {
+    void addUserButtonOnClick(ActionEvent event) throws IOException {
+        Stage adduserstage = new Stage();
+        adduserstage.setResizable(false);
+        adduserstage.initModality(Modality.APPLICATION_MODAL);
+        adduserstage.setTitle("Add User");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addUserDialog.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            adduserstage.setScene(new Scene(root));
+            adduserstage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
     }
 
