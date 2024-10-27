bb29b2cd9f5abee724d9d9e5076f84d6ca99cb08
Do Tien Loc <dotienloc2005@gmail.com>
2024-10-27 20:11:14 +0700

    finish AddBookLendingDialog

--- src/main/java/org/library/btl_oop16_library/Controller/AddBookLendingDialogController.java
+++ src/main/java/org/library/btl_oop16_library/Controller/AddBookLendingDialogController.java
@@ -5,6 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.library.btl_oop16_library.Model.BookLending;
+import org.library.btl_oop16_library.Util.DatabaseConnector;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class AddBookLendingDialogController {
 
@@ -37,7 +42,36 @@ public class AddBookLendingDialogController {
 
     @FXML
     void onConfirmButtonClick(ActionEvent event) {
+        int userID = Integer.parseInt(userIdField.getText());
+        int bookID = Integer.parseInt(bookIDField.getText());
+        int quantity = Integer.parseInt(quantityField.getText());
+        java.util.Date dueDate = null;
+        java.util.Date startDate = null;
+
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+             startDate = df.parse(startDateField.getText());
+             dueDate = df.parse(dueDateField.getText());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        java.sql.Date startDateSQL = new java.sql.Date(startDate.getTime());
+        java.sql.Date dueDateSQL = new java.sql.Date(dueDate.getTime());
+        BookLending bookLending = new BookLending(
+                userID, bookID,
+                startDateSQL,
+                dueDateSQL,
+                quantity);
+        try {
+            DatabaseConnector.addBookLendingToDB(bookLending);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        Stage stage = (Stage) confirmButton.getScene().getWindow();
+        stage.close();
 
     }
-
 }
